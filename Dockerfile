b0d28835c5c9641f640fc70a1eda2e601d1ab332
Quentin C <quentin.cwn@gmail.com>
2025-06-24 23:18:50 +0200

    Add examples & opencv

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,14 @@ RUN echo 'export HOST_TOOLS_DIR=/host-tools' >> ~/.bashrc &&\
     echo 'export WIRINGX_LIB_DIR=/duo-examples/libs/system/musl_riscv64' >> ~/.bashrc &&\
     echo 'export WIRINGX_INCLUDE_DIR=/duo-examples/include/system' >> ~/.bashrc
 
+# Configure OpenCV mobile
+RUN apt-get -y install unzip &&\
+    cd / &&\
+    wget https://github.com/nihui/opencv-mobile/releases/download/v33/opencv-mobile-4.11.0-milkv-duo.zip &&\
+    unzip opencv-mobile-4.11.0-milkv-duo.zip &&\
+    rm opencv-mobile-4.11.0-milkv-duo.zip &&\
+    echo 'export OPENCV_DIR=/opencv-mobile-4.11.0-milkv-duo' >> ~/.bashrc
+
 RUN mkdir /milkv_workspace
 
 WORKDIR /milkv_workspace
--- configure_docker.sh
+++ configure_docker.sh
@@ -8,11 +8,16 @@ if ! [ -f /.dockerenv ]; then
 fi
 
 # Create directory structure
-cd /milkv_workspace
+cd /milkv_workspace/src/MiAM_Robotics_RiscV/examples
 
-mkdir -p build/helloworld
+# Compile examples
+for example_file in */ ; do 
+    example_file="${example_file%/}"     # strip trailing slash
+    example_file="${example_file##*/}"   # strip path and leading slash
+    echo "$example_file"; 
 
-# Compile code
-cd /milkv_workspace/build/helloworld
-cmake /milkv_workspace/src/MiAM_Robotics_RiscV/helloworld -DCMAKE_BUILD_TYPE=Release
-make -j8
+    mkdir -p "/milkv_workspace/build/examples/$example_file"
+    cd "/milkv_workspace/build/examples/$example_file"
+    cmake "/milkv_workspace/src/MiAM_Robotics_RiscV/examples/$example_file" -DCMAKE_BUILD_TYPE=Release
+    make -j8
+done
